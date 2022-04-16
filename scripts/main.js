4fc1fd1d9afda7780748d3010286d8b6388d2a8e
Andres De la Hoz <andelahozg@gmail.com>
2022-04-16 03:51:10 +0000

    Adding initial features

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 import { Student, EducationLevel } from './student.js';
-export const st = new Student("Andru", "Lopez", "avatar.pgn", 25, EducationLevel.POSTGRADUATE);
+export const st = new Student("Andru", "Lopez", "avatar.png", 25, EducationLevel.POSTGRADUATE);
 console.log(st);
+let studentTable = document.getElementById("student");
+showStudentData(st);
+function showStudentData(student) {
+    let tbodyStudent = document.createElement("tbody");
+    tbodyStudent.innerHTML = `<tr><td colspan=2><img src="./${student.avatar}" height="100"></td></tr>
+    <tr><td>Name:</td><td>${student.names}</td></tr>
+    <tr><td>Last Name:</td><td>${student.lastname}</td></tr>
+    <tr><td>Education Level:</td><td>${student.educationLevel}</td></tr>
+    <tr><td>Age:</td><td>${student.age}</td></tr>`;
+    studentTable.appendChild(tbodyStudent);
+}
