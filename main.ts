1e783faf893cf179b57765a28557f721015d3b3e
anfedelahoz <andelahozg@gmail.com>
2022-04-17 22:28:15 -0500

    Styling and final features added

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ import {Student, EducationLevel} from './student.js'
 import {Course} from './course.js'
 
 let courses = [
-new Course("Design and Architecture Principles", 20, 94, true, 2022),
-new Course("Agile in software engineering", 20, 94, true, 2022),
-new Course("Biology", 20, 85, true, 2022),
-new Course("Math", 20, 90, true, 2022),
-new Course("Logic", 20, 82, true, 2022),
-new Course("Signals and Systems", 20, 81, true, 2022),
+new Course("Design and Architecture Principles", 400, 94, true, 2022),
+new Course("Agile in software engineering", 200, 94, true, 2022),
+new Course("Biology", 200, 85, true, 2022),
+new Course("Math", 180, 90, true, 2022),
+new Course("Logic", 140, 50, false, 2022),
+new Course("Signals and Systems", 200, 58, false, 2022),
 ]
 
 export const st = new Student("Andru", "Lopez", "avatar.png", 25, EducationLevel.POSTGRADUATE, courses)
@@ -37,32 +37,35 @@ function filterByName():void{
 function showStudentData(student: Student):void{
     let tbodyStudent = document.createElement("tbody");
     tbodyStudent.innerHTML = `<tr><td colspan=2><img src="./${student.avatar}" height="100"></td></tr>
-    <tr><td>Name:</td><td>${student.names}</td></tr>
-    <tr><td>Last Name:</td><td>${student.lastname}</td></tr>
-    <tr><td>Education Level:</td><td>${student.educationLevel}</td></tr>
-    <tr><td>Age:</td><td>${student.age}</td></tr>`
+    <tr><td><b>Name:</b></td><td>${student.names}</td></tr>
+    <tr><td><b>Last Name:</b></td><td>${student.lastname}</td></tr>
+    <tr><td><b>Education level:</b></td><td>${student.educationLevel}</td></tr>
+    <tr><td><b>Age:</b></td><td>${student.age}</td></tr>`
     studentTable.appendChild(tbodyStudent);
 }
 
 function showStats(student: Student):void{
     let certifiedNumber:number = student.giveCertifiedCourses();
     let trElement:HTMLElement = document.createElement("tr");
-    trElement.innerHTML = `<td><b>Certified courses</b></td><td>${certifiedNumber}</td>`;
+    trElement.innerHTML = `<td><b>Certified courses:</b></td><td>${certifiedNumber}</td>`;
     statsTable.appendChild(trElement);
 
 }
 
 function showCourses(courses: Course[]): void{
     let coursesTbody: HTMLElement = document.createElement("tbody");
+    let state: string[] = courses.map(c => c.score>60?'green':'red');
+    let index: number = 0;
     for(let course of courses)
     {
         let trElement:HTMLElement = document.createElement("tr");
         trElement.innerHTML = `<td>${course.name}</td>
         <td>${course.hours}</td>
-        <td>${course.score}</td>
+        <td style=color:${state[index]}>${course.score}</td>
         <td>${course.certified}</td>
         <td>${course.year}</td>`
         coursesTbody.appendChild(trElement);
+        index++
     }
     coursesTable.appendChild(coursesTbody);
 }
